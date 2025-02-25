52a1f63d2aead485c314dcfa1f13d6bd9106ec27
layan <layanazez@gmail.com>
2025-02-25 09:59:14 +0000

    Add mul function to multiply two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * mul-milti two int
+ * @a:first int
+ * @b:second int
+ */
+int mul(int a, int b){
+
+return(a*b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int mul(int a, int b);
 int _isupper(int c);
-int _isdigit(int c);
 #endif /* MAIN_H */
-
