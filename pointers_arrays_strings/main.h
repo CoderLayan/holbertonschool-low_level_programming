7aa21404fc52bc9174c77f949ae7d6556603b4e8
layan <layanazez@gmail.com>
2025-02-26 10:29:00 +0000

    Fix return statement in 9-strcpy.c to comply with Betty style

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,14 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_array(int *a, int n);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+char *_strcpy(char *dest, char *src);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_rev(char *s);
 
 #endif /* MAIN_H */
+
