10393ac26809c6837ff933587a6bbeafda649961
layan <layanazez@gmail.com>
2025-02-26 09:30:18 +0000

    Implement _puts function and add main header file

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: pointer to the string to be printed
+ * _puts - Prints a string, followed by a new line, to stdout
+ * @str: The string to print
  */
 void _puts(char *str)
 {
 while (*str)
 {
-_putchar(*str++);
+_putchar(*str);
+str++;
 }
 _putchar('\n');
 }
