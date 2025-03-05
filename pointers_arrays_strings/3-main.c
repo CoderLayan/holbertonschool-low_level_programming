38d2516a5d07b55dc179f25f4352b391fb88b050
layan <layanazez@gmail.com>
2025-03-05 10:05:55 +0000

    Add _strspn function to get the length of a prefix substring

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - Check the code.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-char s1[] = "Hello";
-char s2[] = "World!";
-printf("%d\n", _strcmp(s1, s2));  /* Expected Output: -15 */
-printf("%d\n", _strcmp(s2, s1));  /* Expected Output: 15 */
-printf("%d\n", _strcmp(s1, s1));  /* Expected Output: 0 */
+char *s = "hello, world";
+char *f = "oleh";
+unsigned int n;
+n = _strspn(s, f);
+printf("%u\n", n);
 return (0);
 }
