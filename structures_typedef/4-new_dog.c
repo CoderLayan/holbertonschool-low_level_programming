bbae846023b5bac93953da706039f4a1ca7d3928
layan <layanazez@gmail.com>
2025-03-13 14:14:57 +0000

    Implement new_dog function with custom string functions

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,39 +1,70 @@
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 
-/**
-* new_dog - creates a new dog
-* @name: name of the dog
-* @age: age of the dog
-* @owner: owner of the dog
+/*
+* _strlength - Calculates the length of a string.
+* @str: The string whose length is to be calculated.
 *
-* Return: pointer to the new dog (Success), NULL otherwise
+* Return: The length of the string.
+*/
+int _strlength(char *str)
+{
+int len = 0;
+while (str[len] != '\0')
+len++;
+return (len);
+}
+/*
+* _strcopy - Copies a string from source to destination.
+* @dest: Destination buffer.
+* @src: Source string.
+*
+* Return: Pointer to the destination buffer.
+*/
+char *_strcopy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
+}
+/**
+* new_dog - Creates a new dog.
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The name of the dog's owner.
+*
+* Return: Pointer to the new dog_t structure, or NULL if allocation fails.
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *d;
-char *name_copy, *owner_copy;
-d = malloc(sizeof(dog_t));
-if (d == NULL)
+dog_t *new_dog;
+char *new_name, *new_owner;
+/* Allocate memory for the new dog structure */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
 return (NULL);
-name_copy = malloc(strlen(name) + 1);
-if (name_copy == NULL)
+/* Allocate and copy the name */
+new_name = malloc(_strlength(name) + 1);
+if (new_name == NULL)
 {
-free(d);
+free(new_dog);
 return (NULL);
 }
-strcpy(name_copy, name);
-owner_copy = malloc(strlen(owner) + 1);
-if (owner_copy == NULL)
+_strcopy(new_name, name);
+/* Allocate and copy the owner */
+new_owner = malloc(_strlength(owner) + 1);
+if (new_owner == NULL)
 {
-free(name_copy);
-free(d);
+free(new_name);
+free(new_dog);
 return (NULL);
 }
-strcpy(owner_copy, owner);
-d->name = name_copy;
-d->age = age;
-d->owner = owner_copy;
-return (d);
+_strcopy(new_owner, owner);
+/* Initialize the new dog's fields */
+new_dog->name = new_name;
+new_dog->age = age;
+new_dog->owner = new_owner;
+return (new_dog);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stddef.h> /* Ensure this is included for NULL */
-
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
+ * struct dog - A structure to store details about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the dog's owner.
  */
 struct dog
 {
@@ -17,9 +13,11 @@ char *name;
 float age;
 char *owner;
 };
-/* Function prototype */
+
+/* Typedef for struct dog */
 typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/* Function prototypes */
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
