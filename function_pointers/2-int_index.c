c8d2d8c3f7a7a731e47a3d06b28b4aa1570f351b
layan <layanazez@gmail.com>
2025-03-16 10:49:10 +0000

    Implement int_index function with test cases

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+* int_index - Searches for an integer in an array using a comparison function.
+* @array: The array to search through.
+* @size: The size of the array.
+* @cmp: The comparison function to use.
+*
+* Return: The index of the first element for which the cmp function
+*         does not return 0. If no elements match or size <= 0, return -1.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL||cmp == NULL||size <= 0)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1); /* No match found */
+}
