f103fd00c3b4350efeadce9589e3dc74f5e6ee4f
layan <layanazez@gmail.com>
2025-03-29 23:32:53 +0000

    Implement add_node_end function

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,25 @@
 #include "lists.h"
 
 /**
+* _strlen - Custom function to calculate the length of a string.
+* @str: The string whose length is to be calculated.
+*
+* Return: The length of the string, or 0 if the string is NULL.
+*/
+unsigned int _strlen(const char *str)
+{
+unsigned int len = 0;
+
+if (str == NULL) /* Handle NULL string edge case */
+return (0);
+
+while (str[len] != '\0') /* Increment length until null terminator */
+len++;
+
+return (len);
+}
+
+/**
 * add_node_end - Adds a new node at the end of a list_t list.
 * @head: Double pointer to the head of the list.
 * @str: String to be added to the new node.
@@ -13,31 +32,36 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node, *temp;
 
-new_node = malloc(sizeof(list_t)); /* Allocate memory for the new node */
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 
-new_node->str = strdup(str); /* Duplicate the string */
-if (new_node->str == NULL) /* Check for strdup failure */
+/* Duplicate the string and check for failure */
+new_node->str = strdup(str);
+if (new_node->str == NULL)
 {
 free(new_node);
 return (NULL);
 }
 
-new_node->len = strlen(str); /* Set the length of the string */
-new_node->next = NULL;       /* This will be the last node, so next is NULL */
+/* Set the length and next pointer */
+new_node->len = _strlen(str);
+new_node->next = NULL;
 
-if (*head == NULL) /* If the list is empty, make new_node the head */
+/* If the list is empty, make the new node the head */
+if (*head == NULL)
 {
 *head = new_node;
 }
-else /* Traverse to the end of the list and add the new node */
+else
 {
+/* Traverse the list to find the last node */
 temp = *head;
 while (temp->next != NULL)
-{
 temp = temp->next;
-}
+
+/* Point the last node to the new node */
 temp->next = new_node;
 }
 
