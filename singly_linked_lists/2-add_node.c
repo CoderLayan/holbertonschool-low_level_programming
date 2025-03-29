adad8979e9f46bf2fd239aac93dcd7d889288163
layan <layanazez@gmail.com>
2025-03-29 23:15:46 +0000

    Replace strlen with custom _strlen in add_n

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,25 @@
 #include "lists.h"
 
 /**
+* _strlen - Custom function to calculate the length of a string.
+* @str: The string whose length is to be calculated.
+*
+* Return: The length of the string, or 0 if the string is NULL.
+*/
+unsigned int _strlen(const char *str)
+{
+unsigned int len = 0;
+
+if (str == NULL) /* Handle NULL string edge case */
+return (0);
+
+while (str[len] != '\0') /* Increment length until null terminator */
+len++;
+
+return (len);
+}
+
+/**
 * add_node - Adds a new node at the beginning of a list_t list.
 * @head: Double pointer to the head of the list.
 * @str: String to be added to the new node.
@@ -13,20 +32,27 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
 
-new_node = malloc(sizeof(list_t)); /* Allocate memory for the new node */
+/* Allocate memory for the new node */
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 
-new_node->str = strdup(str); /* Duplicate the string */
-if (new_node->str == NULL) /* Check for strdup failure */
+/* Duplicate the string and check for failure */
+new_node->str = strdup(str);
+if (new_node->str == NULL)
 {
 free(new_node);
 return (NULL);
 }
 
-new_node->len = strlen(str); /* Set the length of the string */
-new_node->next = *head;      /* Point new node to the current head */
-*head = new_node;            /* Update head to point to the new node */
+/* Use _strlen to determine the length of the string */
+new_node->len = _strlen(str);
+
+/* Point the new node to the current head of the list */
+new_node->next = *head;
+
+/* Update the head to point to the new node */
+*head = new_node;
 
 return (new_node);
 }
