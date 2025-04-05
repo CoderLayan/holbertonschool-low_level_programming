0b2cb73a2050c76f7d9a25a3d56f8891165bbff9
layan <layanazez@gmail.com>
2025-04-05 14:51:47 +0000

    Implement read_textfile function

--- file_io/main.h
+++ file_io/main.h
@@ -5,22 +5,22 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to standard output.
- * @c: The character to print.
- *
- * Return: On success, returns the number of characters written.
- *         On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - Writes a character to standard output.
+* @c: The character to print.
+*
+* Return: On success, returns the number of characters written.
+*         On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c);
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed,
- *         or 0 on failure.
- */
+* read_textfile - Reads a text file and prints it to the POSIX standard output.
+* @filename: The name of the file to read.
+* @letters: The number of letters to read and print.
+*
+* Return: The actual number of letters read and printed,
+*         or 0 on failure.
+*/
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
