06199170cbfadc59b93bed970a833289808d07d6
layan <layanazez@gmail.com>
2025-04-05 14:46:54 +0000

    Implement read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 /**
 * read_textfile - Reads a text file and prints it to the POSIX standard output.
 * @filename: The name of the file to read.
@@ -15,12 +14,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 int fd;
 ssize_t bytes_read, bytes_written;
 char *buffer;
-
-/* Validate filename */
 if (!filename)
 return (0);
-
-/* Open the file */
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
@@ -30,6 +25,8 @@ if (!buffer)
 close(fd);
 return (0);
 }
+
+/* Read */
 bytes_read = read(fd, buffer, letters);
 if (bytes_read == -1)
 {
@@ -44,8 +41,8 @@ free(buffer);
 close(fd);
 return (0);
 }
+/* Clean up */
 free(buffer);
 close(fd);
-
 return (bytes_written);
 }
